bcbbfe1a62e0835ba179da3a9c9c2db020be1aa2
Philip Skinner <me@philip-skinner.co.uk>
2020-12-01 17:08:24 +0000

    First aoc day

---
+++ day1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day1 {
+    class Program {
+        static void Main(string[] args) {
+            //horribly memory inefficient, wanted to solve it with linq though
+            var lines = File.ReadAllText("list.txt").Split('\n').Select(x => int.Parse(x));
+
+            var twoParts =
+                (
+                    from entry in (
+                        from first in lines
+                        from second in lines
+                        select new {first, second}
+                    )
+                    where entry.first + entry.second == 2020
+                    select entry.first * entry.second
+                ).First()
+            ;
+
+            var threeParts =
+                (
+                    from entry in (
+                        from first in lines
+                        from second in lines
+                        from third in lines
+                        select new {first, second, third}
+                    )
+                    where entry.first + entry.second + entry.third == 2020
+                    select entry.first * entry.second * entry.third
+                ).First()
+            ;
+
+            Console.WriteLine($"Two components: {twoParts}");
+            Console.WriteLine($"Three components: {threeParts}");
+        }
+    }
+}
