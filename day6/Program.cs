97ba80c3b668a4c26f7362ddfb6400c88430fb66
Philip Skinner <me@philip-skinner.co.uk>
2020-12-06 08:47:38 +0000

    Sixth day of aoc

---
+++ day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day6 {
+    class Program {
+    	//nasty
+        static void Main(string[] args) {
+        	var lines = File.ReadAllText("lines.txt").Split("\n\n");
+            var summed = lines.Select(x => x.Replace("\n", "").Distinct().Count()).Sum();
+            int tot = 0;
+
+            foreach (var l in lines) {
+            	var chars = l.Replace("\n", "").Distinct();
+            	var allAnswered = string.Join("", l.Replace("\n", "").Distinct());
+
+            	foreach (var c in chars) {
+	            	foreach (var g in l.Split("\n")) {
+	            		if (g.IndexOf(c) == -1 && !string.IsNullOrWhiteSpace(g)) {
+	            			allAnswered = allAnswered.Replace($"{c}", "");
+	            		}
+	            	}
+            	}
+
+            	Console.WriteLine($"Complete is {l}, answered is {allAnswered}");
+            	tot += allAnswered.Length;
+            }
+
+
+            Console.WriteLine($"Total is {summed}");
+            Console.WriteLine($"Second is {tot}");
+        }
+    }
+}
